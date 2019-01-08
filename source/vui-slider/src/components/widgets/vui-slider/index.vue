9100bcb6b7f570cc05c0b213831b0a76e201bf80
luohanwen <luohanwen@bigo.sg>
2019-01-08 14:52:14 +0800

    vui-slider

--- source/vui-slider/src/components/widgets/vui-slider/index.vue
+++ source/vui-slider/src/components/widgets/vui-slider/index.vue
@@ -1,18 +1,51 @@
 <template>
+    <div>
+          <Slider
+            v-model="realValue"
+            :min="min"
+            :max="max"
+            :step="step"
+            :disabled="disabled"
+            :range="range"
+            :show-input="showInput"
+            :show-stops="showStops"
+            :show-tip="showTip"
+            :tip-format="tipFormat"
+            :input-size="inputSize"
+        ></Slider>
+    </div>
 </template>
 <script>
 export default {
-  name : "vui-slider",
-  props : {
-      widgetCode:{
-          type:String
-      },
+  name: "vui-slider",
+  props: {
+    widgetCode: String,
+    value: [Number, Array, String],
+    min: Number,
+    max: Number,
+    step: Number,
+    disabled: Boolean,
+    range: Boolean,
+    showInput: Boolean,
+    showStops: Boolean,
+    showTip: String,
+    tipFormat: Function,
+    inputSize: String
   },
-  data : function(){
-    return {};
+  data: function() {
+    return {
+      realValue: (()=>{return typeof this.value === "string"?+this.value:this.value;})()
+    };
   },
-  methods : {
-  }
-}
+  watch: {
+    value(val) {
+      this.realValue = typeof val === "string" ? +val : val;
+    },
+    realValue(val) {
+      this.$emit("input", val);
+    }
+  },
+  methods: {}
+};
 </script>
 <style scoped lang="less" src="./theme.less"></style>

5433a5b02697706a1d66f87314cf7433d47d513e
luohanwen <luohanwen@bigo.sg>
2019-05-06 13:34:41 +0800

    vui-slider

--- source/vui-slider/src/components/widgets/vui-slider/index.vue
+++ source/vui-slider/src/components/widgets/vui-slider/index.vue
@@ -1,21 +1,23 @@
 <template>
-  <Slider
-    v-model="realValue"
-    :min="min"
-    :max="max"
-    :step="step"
-    :disabled="disabled"
-    :range="range"
-    :show-input="showInput"
-    :show-stops="showStops"
-    :show-tip="showTip"
-    :tip-format="tipFormat"
-    :input-size="inputSize"
-  ></Slider>
+    <Slider
+        v-model="realValue"
+        :min="min"
+        :max="max"
+        :step="step"
+        :disabled="disabled"
+        :range="range"
+        :show-input="showInput"
+        :show-stops="showStops"
+        :show-tip="showTip"
+        :tip-format="tipFormat"
+        :input-size="inputSize"
+        @on-change="onChange"
+        @on-input="onInput"
+    ></Slider>
 </template>
 <script>
 export default {
-  name: "vui-slider",
+  name: 'vui-slider',
   props: {
     widgetCode: String,
     value: [Number, Array, String],
@@ -46,27 +48,35 @@ export default {
       let initValue = this.value;
       let result;
       if (isRange) {
-        result = typeof initValue === "string" ? val.join(",") : val;
+        result = typeof initValue === 'string' ? val.join(',') : val;
       } else {
-        result = typeof initValue === "string" ? val + "" : val;
+        result = typeof initValue === 'string' ? val + '' : val;
       }
-      this.$emit("input", result);
+      this.$emit('input', result);
     }
+  },
+  mounted() {
+    this.$el.setAttribute('widget-code', this.widgetCode);
   },
   methods: {
     getRealValue(value) {
       let isRange = this.range;
       let result;
       if (isRange) {
-        result =
-          typeof value === "string" ? (value ? value.split(",") : [0,0]) : value;
-          if(result.length<2){
-              result = [0,0];
-          }
+        result = typeof value === 'string' ? (value ? value.split(',') : [0, 0]) : value;
+        if (result.length < 2) {
+          result = [0, 0];
+        }
       } else {
-        result = typeof value === "string" ? +value : value;
+        result = typeof value === 'string' ? +value : value;
       }
       return result;
+    },
+    onChange(val) {
+      this.$emit('on-change', val);
+    },
+    onInput(val) {
+      this.$emit('on-input', val);
     }
   }
 };
